69814aba7cb88a0d3125ebc34eb385859161491c
SteSala <34539912+SteSala@users.noreply.github.com>
2018-05-28 10:38:44 +0200

    Add files via upload

--- Questionario/src/java/Archivio/QuestionarioServlet.java
+++ Questionario/src/java/Archivio/QuestionarioServlet.java
@@ -1,4 +1,3 @@
-
 package Archivio;
 
 import java.util.Random;
@@ -8,31 +7,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author sala.stefano
- */
 public class QuestionarioServlet  extends HttpServlet {
     
     public void doGet(HttpServletRequest request, HttpServletResponse response) 
 			           throws ServletException, java.io.IOException {
 
-        try{	    
-
+        try{
             Questionario q = new Questionario();
             HttpSession session = request.getSession(true);	    
             int contatore = 1;
+            
+            /* sistemare range
             Random rand = new Random();
-            int range = 6 - 10 + 1;
+            int range = 10 - 6 + 1;
             
             //numero casuale tra 6 e 10, ogni tot viene inserita domanda di sicurezza
             int randomNum =  rand.nextInt(range) + 5;
-            
+            */
             
             q.setNumeroDomande(Integer.parseInt(request.getParameter("numeroDomande")));
             q.setNome(request.getParameter("nome"));
             q.calcolaCosto(Integer.parseInt(request.getParameter("numeroDomande")));
-            q.setFrequenzaSic(randomNum);
+            //q.setFrequenzaSic(randomNum);
             
             Utente currentUser = (Utente) (session.getAttribute("currentSessionUser")); 
             System.out.println(currentUser.getEmail());
@@ -43,7 +39,7 @@ public class QuestionarioServlet  extends HttpServlet {
             session.setAttribute("Questionario", q);
             session.setAttribute("contatore", contatore);
             response.sendRedirect("creazioneDomanda.jsp");
-        } 
+        }
 
         catch (Throwable theException){
             System.out.println(theException); 
